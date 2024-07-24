621e89e10b2d938663337d34eda3bdc17a7548a1
Logan Miles <144288661+lcmiles@users.noreply.github.com>
2024-07-24 13:04:03 -0500

    pushing working version

--- lab8/src/App.js
+++ lab8/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ToDoList from './ToDoList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ToDoList />
       </header>
     </div>
   );
 }
-
 export default App;
+ 
+ 
+
+// Sources:
+// https://dev.to/misszamzam/a-simple-todo-list-app-with-react-1bj3
+// https://www.geeksforgeeks.org/create-a-to-do-list-app-using-react-redux/
