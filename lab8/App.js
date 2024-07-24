4d3b93f09a1fac93f8309332d0e6921be8076695
Logan Miles <lcmiles@logansserver.lan>
2024-07-24 09:06:14 -0500

    added react files

---
+++ lab8/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './Styles.css';
+
+import ToDoList from './ToDoList';
+
+function App() {
+
+  return (
+
+    <div className="App">
+
+      <header className="App-header">
+
+        <ToDoList />
+
+      </header>
+
+    </div>
+
+  );
+
+}
+
+export default App;
+ 
